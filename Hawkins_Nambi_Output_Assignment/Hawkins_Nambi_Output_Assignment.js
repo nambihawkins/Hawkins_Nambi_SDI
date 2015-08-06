5fedb7f9b52f73097e3b292f521cba818136ca45
nambihawkins <nambihawkins@fullsail.edu>
2015-08-06 18:19:23 -0400

    Adding Comments
    Added Name, Class, Term, Assignment to script

--- Hawkins_Nambi_Output_Assignment/Hawkins_Nambi_Output_Assignment.js
+++ Hawkins_Nambi_Output_Assignment/Hawkins_Nambi_Output_Assignment.js
@@ -1,6 +1,11 @@
 /**
  * Created by Nambi on 8/6/15.
  */
+//Name: Nambi Hawkins
+//Class: Scalable Data Infrastructure
+//Term: August 2015, Section 01
+//Output Assignment
+
 //I am stating the District I was born
 var birthPlace("Washington DC");
-//I am stating my 
+//
