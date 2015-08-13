aaaa9716416da486068ea9e7243114bfa673397b
nambihawkins <nambihawkins@fullsail.edu>
2015-08-13 19:35:44 -0400

    Added script for first part of problem
    Trying to determine how many mph Tyler has to run for his final race in
    order to get his average at 12mph.

--- Hawkins_Nambi_Expressions_Assignment/js/script.js
+++ Hawkins_Nambi_Expressions_Assignment/js/script.js
@@ -1,3 +1,10 @@
-/**
- * Created by Nambi on 8/12/15.
- */
+var recordedTimes = [12, 11, 12, 11, 13, 14, 9];
+var goal = 12;
+var numberOfRecordedTimes = recordedTimes.length;
+
+recordedTimesTotal = recordedTimes[0] + recordedTimes[1] + recordedTimes[2] + recordedTimes[3] + recordedTimes[4] + recordedTimes[5] + recordedTimes[6];
+
+numberOfRecordedTimes ++;
+
+target = goal * numberOfRecordedTimes - recordedTimesTotal;
+console.log("In order for Tyler to have hit his goal of" + " " + goal + "mph, he would have had to run" + " " + target + "mph in his final race.");
