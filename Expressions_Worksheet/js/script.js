472972ce5cb451bda16b2f7bd0cef3d5ec08cddc
nambihawkins <nambihawkins@fullsail.edu>
2015-08-12 21:12:51 -0400

    Set variables and solve for circumference, printing out the result
    Set variables and solve for circumference, printing out the result

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -7,3 +7,9 @@ var length = 6;
 var width = 7;
 var areaOfRectangle = length*width;
 console.log(areaOfRectangle);
+
+//Calculating the circumference of a circle
+var radius = 6; //set radius value
+const PI = 3.14159265; //set value for PI
+var circumference = 2*PI*radius; //equation to solve for circumference of a circle
+console.log("The circumference of the circle is " + circumference + " " + "sq units"); //print circumference with units
