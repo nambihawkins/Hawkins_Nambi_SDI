e1a298948c383f90f5de7416239c53c93da33a77
nambihawkins <nambihawkins@fullsail.edu>
2015-08-12 21:04:23 -0400

    Set variables and display
    set the variables for length and width of rectangle as well as the
    equation for solving the area of a rectangle, then set the console to
    display the answer

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -2,4 +2,8 @@
 //Wednesday, August 12, 2015
 //Expressions Worksheet
 
-
+//Calculating the area of a rectangle.
+var length = 6;
+var width = 7;
+var areaOfRectangle = length*width;
+console.log(areaOfRectangle);
