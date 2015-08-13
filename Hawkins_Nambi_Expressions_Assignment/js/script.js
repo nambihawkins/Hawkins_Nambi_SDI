4d31d8910bce5c29bee8f23a8a2d59cf22699aa1
nambihawkins <nambihawkins@fullsail.edu>
2015-08-13 19:47:41 -0400

    Add comments explaining each line
    Add comments explaining each line

--- Hawkins_Nambi_Expressions_Assignment/js/script.js
+++ Hawkins_Nambi_Expressions_Assignment/js/script.js
@@ -1,10 +1,14 @@
-var recordedTimes = [12, 11, 12, 11, 13, 14, 9];
-var goal = 12;
-var numberOfRecordedTimes = recordedTimes.length;
+//Nambi Hawkins
+//August 13, 2015
+//Expressions Assignment
 
-recordedTimesTotal = recordedTimes[0] + recordedTimes[1] + recordedTimes[2] + recordedTimes[3] + recordedTimes[4] + recordedTimes[5] + recordedTimes[6];
+var recordedTimes = [12, 11, 12, 11, 13, 14, 9]; //Entered in the Tyler's weekly times
+var goal = 12; //set goal speed in order to get accepted into college
+var numberOfRecordedTimes = recordedTimes.length; //found number of pieces of data in the array
 
-numberOfRecordedTimes ++;
+recordedTimesTotal = recordedTimes[0] + recordedTimes[1] + recordedTimes[2] + recordedTimes[3] + recordedTimes[4] + recordedTimes[5] + recordedTimes[6]; //totalled up the times in the array
 
-target = goal * numberOfRecordedTimes - recordedTimesTotal;
-console.log("In order for Tyler to have hit his goal of" + " " + goal + "mph, he would have had to run" + " " + target + "mph in his final race.");
+numberOfRecordedTimes ++; //added 1 to the original pieces of data in array to account for missing time
+
+target = goal * numberOfRecordedTimes - recordedTimesTotal; //calculated time Tyler needed to reach for his last run in order to meet his goal
+console.log("In order for Tyler to have hit his goal of" + " " + goal + "mph, he would have had to run" + " " + target + "mph in his final race."); //concatenate results into a sentence explaining the outcome needed for Tyler to reach his goal
