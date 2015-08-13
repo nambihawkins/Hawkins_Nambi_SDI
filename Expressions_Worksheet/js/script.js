bcc9f26a01e976256bbedd28956a52013e96e460
nambihawkins <nambihawkins@fullsail.edu>
2015-08-12 21:20:48 -0400

    Commented out previous problems and solved for sparky's age in Dog Years
    Commented out previous problems and solved for sparky's age in Dog Years

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -3,13 +3,19 @@
 //Expressions Worksheet
 
 //Calculating the area of a rectangle.
-var length = 6;
-var width = 7;
-var areaOfRectangle = length*width;
-console.log(areaOfRectangle);
+//var length = 6;
+//var width = 7;
+//var areaOfRectangle = length*width;
+//console.log(areaOfRectangle);
 
 //Calculating the circumference of a circle
-var radius = 6; //set radius value
-const PI = 3.14159265; //set value for PI
-var circumference = 2*PI*radius; //equation to solve for circumference of a circle
-console.log("The circumference of the circle is " + circumference + " " + "sq units"); //print circumference with units
+//var radius = 6; //set radius value
+//const PI = 3.14159265; //set value for PI
+//var circumference = 2*PI*radius; //equation to solve for circumference of a circle
+//console.log("The circumference of the circle is " + circumference + " " + "sq units"); //print circumference with units
+
+//Dog Years
+var humanYears = 1;
+var dogYears = 7;
+var age = humanYears*dogYears;
+console.log("Sparky is" + " " + humanYears + " " + "human years old which is" + " " + age + " " + "in dog years.");
