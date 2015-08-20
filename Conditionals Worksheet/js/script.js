409c1d3e194684d4994023028b35a632fecd8caa
nambihawkins <nambihawkins@fullsail.edu>
2015-08-20 18:26:46 -0400

    Example 1
    Comments and code provided for example 1

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -1,3 +1,23 @@
-/**
- * Created by Nambi on 8/20/15.
- */
+//To get in the heavyweight division of the Strawberry Festival’s pie eating contest you must weigh 250 lbs or more.
+// Determine whether an entrant qualifies based on his weight.
+
+//Given:
+//Competitor’s weight.
+
+//Result:
+//“The competitor qualifies for the heavyweight division.” Or  “ The competitor needs to gain some weight!”
+
+//declaring myWeight as a variable with the value of 214
+//var myWeight = 214;
+
+//qualifying = (myWeight >= 250) ? "The competitor qualifies for the heavyweight division." : "The competitor needs to gain some weight!";
+//setting up a ternary operation for the problem, setting the answer to qualifying
+
+//printing out the result to console
+//console.log(qualifying);
+
+//
+
+
+
+
