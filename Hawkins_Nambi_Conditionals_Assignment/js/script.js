68579556f28ee370ab8f205907c5bed50d24603f
nambihawkins <nambihawkins@fullsail.edu>
2015-08-20 21:47:32 -0400

    Stating the problem we are solving
    Stating the problem we are solving

---
+++ Hawkins_Nambi_Conditionals_Assignment/js/script.js
@@ -0,0 +1,9 @@
+//Nambi Hawkins
+//August 20, 2015
+//Conditonals Assignment
+
+prompt("You work for a chute manufacturing company as an estimator. You need to know which company to use for freight. " +
+    "If a chute height is 55’ or less, you can use YRC, and it doesn’t matter how many chutes you are shipping as " +
+    "long as they are all 55’ or less. If a chute is greater than 55’, you need to use C.H. Robinson.");
+
+//stating the problem
