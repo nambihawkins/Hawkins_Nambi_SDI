dead21c960420da3ab3fbeaa5b1071360cd154eb
nambihawkins <nambihawkins@fullsail.edu>
2015-08-20 22:01:20 -0400

    asking for chute type
    chute type calculates number of pallets needed

--- Hawkins_Nambi_Conditionals_Assignment/js/script.js
+++ Hawkins_Nambi_Conditionals_Assignment/js/script.js
@@ -10,3 +10,14 @@ alert("You work for a chute manufacturing company as an estimator. You need to k
 var name = prompt("Thank you for visiting Freight Helper. My name is Rachel, yours is?");
 //Make the experience personal by asking for the name and using it.
 
+var chuteType = prompt("Welcome, " + name + " will you be shipping a trash or linen chute with us today?");
+
+
+//Asking user what type of chute they are shipping.
+
+if (chuteType == "trash"){
+    pallets = 2;
+} else {
+    pallets = 3;
+}
+//A trash chute gets 2 pallets, a linen chute gets 3
