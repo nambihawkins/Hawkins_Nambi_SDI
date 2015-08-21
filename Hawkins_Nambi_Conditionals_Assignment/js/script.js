c44d0de8fe530e8b7b482d2d2113e0712c10f48c
nambihawkins <nambihawkins@fullsail.edu>
2015-08-20 21:53:02 -0400

    personalize the experience by asking for a name
    personalize the experience by asking for a name

--- Hawkins_Nambi_Conditionals_Assignment/js/script.js
+++ Hawkins_Nambi_Conditionals_Assignment/js/script.js
@@ -3,7 +3,10 @@
 //Conditonals Assignment
 
 alert("You work for a chute manufacturing company as an estimator. You need to know which company to use for freight. " +
-    "If a chute height is 55’ or less, you can use YRC, and it doesn’t matter how many chutes you are shipping as " +
-    "long as they are all 55’ or less. If a chute is greater than 55’, you need to use C.H. Robinson.");
-
+    "If a chute height is 55' or less, you can use YRC, and it doesn't matter how many chutes you are shipping as " +
+    "long as they are all 55' or less. If a chute is greater than 55', you need to use C.H. Robinson.");
 //stating the problem
+
+var name = prompt("Thank you for visiting Freight Helper. My name is Rachel, yours is?");
+//Make the experience personal by asking for the name and using it.
+
