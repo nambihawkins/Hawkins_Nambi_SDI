69aea2415cd42ffc8b7b8e49ac579b06c75a987b
nambihawkins <nambihawkins@fullsail.edu>
2015-08-27 23:19:29 -0400

    Prompting the user
    Prompting the user to select which group of numbers we will run code for

---
+++ Hawkins_Nambi_Functions_Assignment/js/script.js
@@ -0,0 +1,7 @@
+//Nambi Hawkins
+//Scalable Data Infrastructures, August Section 1
+//August 27, 2015
+//Functions Assignment
+
+var userOption = prompt("Would you like to run Powerball Lottery numbers? \nType Yes to play. Leave blank for Florida Lottery!");
+//asking user which set of random numbers they would like displayed and setting their option as a variable
